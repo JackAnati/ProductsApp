0cd8c5c0c2d71a4f326f3e42f6871f8329674fe6
Ace <Ace@DESKTOP-E7ERJBG>
2025-12-16 16:41:41 +0200

    Add project files.

---
+++ ProductApp/Controllers/ProductController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Tasks.Deployment.Bootstrapper;
+using ProductApp.Data;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Listing all products in a database
+        public IActionResult Index()
+        {
+            var productList = _context.Products.ToList();
+            return View(productList);
+        }
+        
+        //The function is for allowing for the creation of an html view
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //The function is for creating a product
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Models.Product product) 
+        {
+
+            if (ModelState.IsValid) 
+            {
+                product.CreatedDate = DateTime.UtcNow; // Defaulting the time to be now.
+
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            return View(product);
+        
+        }
+        //The function is for allowing for the creation of an html view for updating a product
+        [HttpGet]
+        public IActionResult Edit(int? id) 
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var product = _context.Products.FirstOrDefault(e => e.ProductId == id);
+            if (product == null) 
+            {
+                return NotFound();
+                    
+            }
+            return View(product);
+        }
+        //The function is for updating a product
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,Models.Product product)
+        {
+            if(id != product.ProductId)
+            {
+                return NotFound();
+            }
+            
+            if (ModelState.IsValid)
+            {
+                product.CreatedDate = DateTime.UtcNow;
+
+                _context.Products.Update(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            return View(product);
+
+        }
+        // After a user has confirmed the deletion of a product the following function will be triggered based on the productId
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ ProductApp/wwwroot/js/script.js
@@ -0,0 +1,23 @@
+﻿const submitBtn = document.getElementById("btn-save");
+const deleteModal = document.getElementById("deleteModal");
+const confirmDelete = document.getElementById("confirmDeleteBtn");
+const name = document.getElementById("name").value.trim();
+const price = document.getElementById("price").value.trim();
+const description = document.getElementById("description").value.trim();
+
+submitBtn.addEventListener("click", function (event) {
+   
+    if (name === "" || price === "" || description === "") {
+        event.preventDefault();
+        alert("Please fill in all fields");
+    }
+});
+
+function openDeleteModal(productId) {
+    deleteModal.style.display = "block";
+    confirmDelete.href = "/Product/Delete/" + productId;
+}
+
+function closeDeleteModal() {
+    deleteModal.style.display = "none";
+}
